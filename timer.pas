6ece57604ac2c7c74b4c57fbc83fcbab57240ed7
Никита Пономарев <ponomarevnikita28674@gmail.com>
2024-10-05 09:30:01 +0300

    Add 2 files: time.pas and timer.pas

---
+++ timer.pas
@@ -0,0 +1,14 @@
+﻿Program P3;
+begin
+  var sum := 0;
+  var hours := ReadInteger('Введите кол-во часов:');
+  Assert(hours > 0);
+  sum += 3600 * hours;
+  var minutes := ReadInteger('Введите кол-во минут:');
+  Assert(hours > 0);
+  sum += 60 * minutes;
+  var seconds := ReadInteger('Введите кол-во секунд:');
+  Assert(seconds > 0);
+  sum += seconds;
+  Print($'Кол-во секунд: {sum}')
+end.
